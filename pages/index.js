4d3903fd8e538074da10067a1f7c553db01cd0e7
Alan W. Smith <alan@alanwsmith.com>
2021-11-20 15:20:00 -0500

    Minor update to force push

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div>surftech.tv</div>
+  return <div>-- surftech.tv --</div>
 }
