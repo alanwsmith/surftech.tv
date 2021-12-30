cd8cf60b9475a0df2a3ec6b97e312f931ff14f41
Alan W. Smith <alan@alanwsmith.com>
2021-12-29 20:48:46 -0500

    Basic video player in place

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # surftech.tv
 
 The source code for [surftech.tv](https://www.surftech.tv/)
+
+Initial source for videos:
+
+https://www.youtube.com/watch?v=kYAtt3CUN-Y&list=PLkn-t62Aed1AGx6821DPGVD8yElBP1SVf
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
-  return <div>- surftech.tv -</div>
+  const videoList = ['kYAtt3CUN-Y', 'JDdR94w8-7Q']
+
+  function randomItem(theArray) {
+    const randomNumber = Math.floor(Math.random() * theArray.length)
+    return theArray[randomNumber]
+  }
+
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      {randomItem(videoList)}
+      <div className="videoWrapper_16x9">
+        <iframe
+          width="560"
+          height="315"
+          src={`http://www.youtube.com/embed/${randomItem(videoList)}`}
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  )
 }
