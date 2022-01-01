56fcf7dffcc8837e7630dce59bec045195f82c36
Alan W. Smith <alan@alanwsmith.com>
2021-12-31 22:52:21 -0500

    Setup to load a random video

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,22 @@ var player
 
 export default function Home() {
   const [initialized, setInitialized] = useState(false)
+  const [playerWrapper, setPlayerWrapper] = useState({})
   useEffect(() => {
     if (!initialized) {
+      const tmpVideoIds = [
+        'kYAtt3CUN-Y',
+        'JDdR94w8-7Q',
+        'JtwHtfFe6AI',
+        '6JNwK6hEneg',
+        'f7s8_PGpVT0',
+        'R-YX_7-7dm0',
+        'lQLsswJHa_c',
+        'DC9032_nkyc',
+        'Jk75JtXACL8',
+        'woXnRQR8dQ4',
+        'ELdFoIYTBL8',
+      ]
       setInitialized(true)
       console.log('Initializing')
       var tag = document.createElement('script')
@@ -14,7 +28,7 @@ export default function Home() {
         player = new window.YT.Player('player', {
           width: 640,
           height: 390,
-          videoId: 'M7lc1UVf-VE',
+          videoId: tmpVideoIds[Math.floor(Math.random() * tmpVideoIds.length)],
           events: {
             onReady: handlePlayerReady,
             onStateChange: handlePlayerStateChange,
@@ -26,13 +40,20 @@ export default function Home() {
     }
   }, [initialized])
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log('here')
-      console.log(player.getCurrentTime())
-    }, 2e3)
-    return () => clearInterval(interval)
-  })
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     markTime()
+  //   }, 2e3)
+  //   return () => clearInterval(interval)
+  // })
+
+  function markTime() {
+    console.log('here')
+
+    // if (typeof player !== 'undefined') {
+    //   console.log(player.getCurrentTime())
+    // }
+  }
 
   function handlePlayerReady() {
     console.log('Player Ready')
@@ -40,7 +61,7 @@ export default function Home() {
 
   function handlePlayerStateChange() {
     console.log('state change')
-    console.log(player.getCurrentTime())
+    // console.log(player.getCurrentTime())
   }
 
   if (typeof window !== 'undefined') {
@@ -49,10 +70,14 @@ export default function Home() {
 
   return (
     <>
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div className="videoWrapper_16x9">
-          <div id="player"></div>
+      <div className="max-w-2xl mx-auto">
+        <h1>SurfTech.tv</h1>
+        <div className="max-w-prose mx-auto border-2 border-blue-200 bg-black shadow-md overflow-hidden">
+          <div className="videoWrapper_16x9">
+            <div id="player"></div>
+          </div>
         </div>
+        <p>Refresh to get a new random tech talk video</p>
       </div>
     </>
   )
@@ -62,26 +87,4 @@ export default function Home() {
     const randomNumber = Math.floor(Math.random() * theArray.length)
     return theArray[randomNumber]
   }
-
-  function testId(event, data) {
-    console.log('I AM EHREREE')
-    console.log(event.target.getCurrentTime())
-  }
-
-  return (
-    <>
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        {randomItem(videoList)}
-        <div className="videoWrapper_16x9">
-          <iframe
-            width="560"
-            height="315"
-            src={`http://www.youtube.com/embed/${randomItem(videoList)}`}
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-      </div>
-    </>
-  )
 }
